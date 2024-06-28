a3f6bcf2c193794274c4e79cadbf56e9825bae5a
iiekezie <iiekezie@gmail.com>
2024-06-28 07:27:57 -0700

    Configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+reate a file in /tmp
+
+file { 'holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
